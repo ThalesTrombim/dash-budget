38d4d4b715f45d432bb631b70bea6fec707c9ae1
thales <thalestr@outlook.com>
2024-06-26 20:14:01 -0300

    feat: add firebase

--- src/components/AddExpense/index.tsx
+++ src/components/AddExpense/index.tsx
@@ -1,31 +1,106 @@
+import { useState } from "react";
 import style from "./style.module.scss";
 
+import { addDoc, collection } from "firebase/firestore";
+import { db } from "../../firebase.config";
+
 function AddExpense() {
+  // const [newExpense, setNewExpense] = useState<any>();
+  const [expenseName, setExpenseName] = useState<string>('');
+  const [expenseAmount, setExpenseAmount] = useState<number>(0);
+  const [expenseCategory, setExpenseCategory] = useState<string>('TESTES');
+  const [expenseDateToday, setExpenseDateToday] = useState<boolean>(false);
+  const [expenseDate, setExpenseDate] = useState<Date>();
+
+  
+
+  const newExpense = {
+    name: expenseName,
+    amount: expenseAmount,
+    category: expenseCategory,
+    date: "FUNCAO PARA FORMATAR A DATA",
+  }
+
+  const expenseDateFormated = (date?: Date) => {
+    if(expenseDateToday) return new Date().getTime();
+    if(date) return date.getTime();
+  };
+
+  async function handleAddItem() {
+    const { name, amount, date } = newExpense;
+    const expenseCollection = collection(db, 'expenses');
+  
+    await addDoc(expenseCollection, {
+      name,
+      amount, 
+      date
+    })
+  }
+
+  function handleSubmit(e:any) {
+    e.preventDefault();
+
+    console.log('timestamp date', expenseDateFormated(expenseDate))
+    console.log(expenseName);
+    console.log(expenseAmount);
+    console.log(expenseCategory);
+    console.log(expenseDateToday);
+    console.log(expenseDate);
+
+    handleAddItem();
+  }
+
   return (
     <div className={style.addExpenseContainer}>
       <h3>Adicionar compra</h3>
 
-      <div className={style.addExpenseInputs}>
+      <form onSubmit={handleSubmit} className={style.addExpenseInputs}>
         <div className={style.addExpenseInput}>
           <label htmlFor="item_name">Nome</label>
-          <input type="text" name="item_name" id="item_name" />
+          <input 
+            type="text" 
+            name="item_name" 
+            id="item_name" 
+            value={expenseName} 
+            onChange={(e) => setExpenseName(e.target.value)}
+          />
         </div>
 
         <div className={style.addExpenseInput}>
           <label htmlFor="item_amount">Valor</label>
-          <input type="number" name="item_amount" id="item_amount" />
+          <input 
+            type="number" 
+            name="item_amount" 
+            id="item_amount"
+            value={expenseAmount}
+            onChange={(e) => setExpenseAmount(parseFloat(e.target.value))}
+          />
         </div>
 
         <div className={style.addExpenseCheckbox}>
-          <input type="checkbox" name="item_date_today" id="item_date_today" />
+          <input 
+            type="checkbox"
+            name="item_date_today" 
+            id="item_date_today" 
+            checked={expenseDateToday} 
+            onChange={(e) => setExpenseDateToday(e.target.checked)}
+          />
           <label htmlFor="item_date_today">Data de Hoje?</label>
         </div>
 
         <div className={style.addExpenseInput}>
           <label htmlFor="item_date">Data</label>
-          <input type="date" name="item_date" id="item_date" />
+          <input 
+            type="date" 
+            name="item_date" 
+            id="item_date" 
+            // value={expenseDate} 
+            onChange={(e) => setExpenseDate(new Date(e.target.value))}
+          />
         </div>
-      </div>
+
+        <button type="submit">Adicionar</button>
+      </form>
     </div>
   )
 }
---
+++ src/firebase.config.ts
@@ -0,0 +1,24 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB7LEGmtIFsrVd73f0gHuK-Mf75gd2Cph0",
+  authDomain: "dash-apartment.firebaseapp.com",
+  projectId: "dash-apartment",
+  storageBucket: "dash-apartment.appspot.com",
+  messagingSenderId: "938995189755",
+  appId: "1:938995189755:web:cb4f22ec5064dee45d6d5e"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+const db = getFirestore(app);
+// const auth = getAuth();
+
+export { db };
