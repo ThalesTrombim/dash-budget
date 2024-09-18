33b4fb02f4018972b9f5a2fc9180a4171c67f500
Thales Trombim <thalestr@outlook.com>
2024-09-18 17:40:48 -0300

    style: update add expense page css

--- src/components/AddExpense/index.tsx
+++ src/components/AddExpense/index.tsx
@@ -63,63 +63,66 @@ function AddExpense({ data }: {data: any}) {
 
   return (
     <div className={style.addExpenseContainer}>
-      <div className={style.addExpenseFormArea}>
-        <h3>Adicionar compra</h3>
+      <div className={style.addExpenseContent}>
 
-        <form onSubmit={handleSubmit} className={style.addExpenseInputs}>
-          <div className={style.addExpenseInput}>
-            <label htmlFor="item_name">Nome</label>
-            <input 
-              type="text" 
-              name="item_name" 
-              id="item_name" 
-              value={expenseName} 
-              onChange={(e) => setExpenseName(e.target.value)}
-            />
-          </div>
+        <div className={style.addExpenseFormArea}>
+          <h3>Adicionar compra</h3>
 
-          <div className={style.addExpenseInput}>
-            <label htmlFor="item_amount">Valor</label>
-            <CurrencyInput
-              onChangeValue={(_, originalValue) => {
-                setExpenseAmount(originalValue);
-              }}
-              value={expenseAmount}
-            />
-          </div>
+          <form onSubmit={handleSubmit} className={style.addExpenseInputs}>
+            <div className={style.addExpenseInput}>
+              <label htmlFor="item_name">Nome</label>
+              <input 
+                type="text" 
+                name="item_name" 
+                id="item_name" 
+                value={expenseName} 
+                onChange={(e) => setExpenseName(e.target.value)}
+              />
+            </div>
 
-          <div className={style.addExpenseInput}>
-            <label htmlFor="item_category">Categoria</label>
-            <select onChange={(e) => setExpenseCategory(parseFloat(e.target.value))} name="item_category" id="item_category">
-              {data.map((category: any) => (
-                <option value={category.categoryId}>{category.name}</option>
-              ))}
-            </select>
-          </div>
+            <div className={style.addExpenseInput}>
+              <label htmlFor="item_amount">Valor</label>
+              <CurrencyInput
+                onChangeValue={(_, originalValue) => {
+                  setExpenseAmount(originalValue);
+                }}
+                value={expenseAmount}
+              />
+            </div>
 
-          <div className={style.addExpenseInput}>
-            <label htmlFor="item_date">Data</label>
-            <input 
-              type="date"
-              name="item_date" 
-              id="item_date"
-              onChange={(e) => setExpenseDate(new Date(e.target.value))}
-            />
-          </div>
+            <div className={style.addExpenseInput}>
+              <label htmlFor="item_category">Categoria</label>
+              <select onChange={(e) => setExpenseCategory(parseFloat(e.target.value))} name="item_category" id="item_category">
+                {data.map((category: any) => (
+                  <option value={category.categoryId}>{category.name}</option>
+                ))}
+              </select>
+            </div>
 
-          <div className={style.addExpenseCheckbox}>
-            <input 
-              type="checkbox"
-              name="item_date_today" 
-              id="item_date_today" 
-              checked={expenseDateToday} 
-              onChange={(e) => setExpenseDateToday(e.target.checked)}
-            />
-            <label htmlFor="item_date_today">Data de Hoje?</label>
-          </div>
+            <div className={style.addExpenseInput}>
+              <label htmlFor="item_date">Data</label>
+              <input 
+                type="date"
+                name="item_date" 
+                id="item_date"
+                onChange={(e) => setExpenseDate(new Date(e.target.value))}
+              />
+            </div>
 
-          <button type="submit">Adicionar</button>
-        </form>
+            <div className={style.addExpenseCheckbox}>
+              <input 
+                type="checkbox"
+                name="item_date_today" 
+                id="item_date_today" 
+                checked={expenseDateToday} 
+                onChange={(e) => setExpenseDateToday(e.target.checked)}
+              />
+              <label htmlFor="item_date_today">Data de Hoje?</label>
+            </div>
+
+            <button className={style.addExpenseSubmitButton} type="submit">Adicionar</button>
+          </form>
+        </div>
       </div>
     </div>
   )
