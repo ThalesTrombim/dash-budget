84b1a9bc9154f3a514d8b390871a387164ec6776
Thales Trombim <thalestr@outlook.com>
2024-10-19 18:36:45 -0300

    feat: add reachart to piechart

--- src/components/PieChart/index.tsx
+++ src/components/PieChart/index.tsx
@@ -2,9 +2,11 @@ import Chart from "react-google-charts";
 
 import style from './style.module.scss';
 import { useCategory } from "../../hooks/useCategories";
+import { Cell, Pie, PieChart, ResponsiveContainer, Tooltip } from "recharts";
 
-function PieChart() {
-  const { colorsOnly, categoriesByAmount } = useCategory();
+function PieChartComponent() {
+  const { colorsOnly, categoriesByAmount, expensesByCategory} = useCategory();
+
 
   const options = {
     title: "Gastos por categoria",
@@ -25,18 +27,61 @@ function PieChart() {
     }
   };
 
+  const data02 = [
+    { name: "Group A", value: 2400, color: "#0000ff"},
+    { name: "Group B", value: 4567, color: "#0000ff"},
+    { name: "Group C", value: 1398, color: "#0000ff"},
+    { name: "Group D", value: 9800, color: "#0000ff"},
+    { name: "Group E", value: 3908, color: "#0000ff"},
+    { name: "Group F", value: 4800, color: "#0000ff"}
+  ];
+
+
+  const RADIAN = Math.PI / 180;
+  const renderCustomizedLabel = ({ cx, cy, midAngle, innerRadius, outerRadius, percent, index }: any) => {
+    const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
+    const x = cx + radius * Math.cos(-midAngle * RADIAN);
+    const y = cy + radius * Math.sin(-midAngle * RADIAN);
+  
+    return (
+      <text x={x} y={y} fill="white" textAnchor={x > cx ? 'start' : 'end'} dominantBaseline="central">
+        { percent * 100 >= 5 && `${(percent * 100).toFixed(0)}%`}
+      </text>
+    );
+  };
+
   return (
-    <div className={style.pieChartContainer}>
-      <Chart
-        className={style.chart}
-        chartType="PieChart"
-        width={"100%"}
-        height={"100%"}
-        data={categoriesByAmount}
-        options={options}
-      />
-    </div>
+    // <div className={style.pieChartContainer}>
+    //   <Chart
+    //     className={style.chart}
+    //     chartType="PieChart"
+    //     width={"100%"}
+    //     height={"100%"}
+    //     data={categoriesByAmount}
+    //     options={options}
+    //   />
+    // </div>
+    <ResponsiveContainer >
+      <PieChart>
+        <Pie
+          dataKey="totalAmount"
+          data={expensesByCategory}
+          labelLine={false}
+          label={renderCustomizedLabel}
+          // cx={500}
+          // cy={200}
+          innerRadius={40}
+          outerRadius={130}
+          // fill="#82ca9d"
+        >
+          {expensesByCategory.map((_: any, index: any) => (
+            <Cell key={`cell-${index}`} fill={expensesByCategory[index].fill} />
+          ))}
+        </Pie>
+        <Tooltip />
+      </PieChart>
+    </ResponsiveContainer>
   )
 }
 
-export default PieChart;
+export default PieChartComponent;
