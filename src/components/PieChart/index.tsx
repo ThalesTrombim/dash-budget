2d759959749149c75ee9cf1aa9af723a1118c514
Thales Trombim <thalestr@outlook.com>
2024-11-01 14:28:15 -0300

    feat: add new pie chart from recharts

--- src/components/PieChart/index.tsx
+++ src/components/PieChart/index.tsx
@@ -1,41 +1,8 @@
-import Chart from "react-google-charts";
-
-import style from './style.module.scss';
 import { useCategory } from "../../hooks/useCategories";
 import { Cell, Pie, PieChart, ResponsiveContainer, Tooltip } from "recharts";
 
 function PieChartComponent() {
-  const { colorsOnly, categoriesByAmount, expensesByCategory} = useCategory();
-
-
-  const options = {
-    title: "Gastos por categoria",
-    pieHole: 0.3,
-    is3D: false,
-    colors: colorsOnly,
-    titleTextStyle: {
-      fontSize: 20
-    },
-    legend: {
-      position: 'bottom'
-    },
-    scrollArrows: false,
-    maxLines: 5,
-    chartArea:{
-      left: 16,
-      right: 16,
-    }
-  };
-
-  const data02 = [
-    { name: "Group A", value: 2400, color: "#0000ff"},
-    { name: "Group B", value: 4567, color: "#0000ff"},
-    { name: "Group C", value: 1398, color: "#0000ff"},
-    { name: "Group D", value: 9800, color: "#0000ff"},
-    { name: "Group E", value: 3908, color: "#0000ff"},
-    { name: "Group F", value: 4800, color: "#0000ff"}
-  ];
-
+  const { expensesByCategory} = useCategory();
 
   const RADIAN = Math.PI / 180;
   const renderCustomizedLabel = ({ cx, cy, midAngle, innerRadius, outerRadius, percent, index }: any) => {
@@ -51,36 +18,36 @@ function PieChartComponent() {
   };
 
   return (
-    // <div className={style.pieChartContainer}>
-    //   <Chart
-    //     className={style.chart}
-    //     chartType="PieChart"
-    //     width={"100%"}
-    //     height={"100%"}
-    //     data={categoriesByAmount}
-    //     options={options}
-    //   />
-    // </div>
-    <ResponsiveContainer >
-      <PieChart>
-        <Pie
-          dataKey="totalAmount"
-          data={expensesByCategory}
-          labelLine={false}
-          label={renderCustomizedLabel}
-          // cx={500}
-          // cy={200}
-          innerRadius={40}
-          outerRadius={130}
-          // fill="#82ca9d"
+    <div className="flex flex-col w-full max-h-[360px] h-full p-4 bg-white rounded-lg">
+      <div>
+        <div className='text-start text-lg font-bold'>
+          <h4>Porcentagem por categorias</h4>
+        </div>
+      </div>
+      
+      <ResponsiveContainer width={"100%"} height={"100%"}>
+        <PieChart
+          margin={{
+            top: 16
+          }}
+          title='Porcentagem por categorias'
         >
-          {expensesByCategory.map((_: any, index: any) => (
-            <Cell key={`cell-${index}`} fill={expensesByCategory[index].fill} />
-          ))}
-        </Pie>
-        <Tooltip />
-      </PieChart>
-    </ResponsiveContainer>
+          <Pie
+            dataKey="totalAmount"
+            data={expensesByCategory}
+            labelLine={false}
+            label={renderCustomizedLabel}
+            cy={"30%"}
+            innerRadius={20}
+            >
+            {expensesByCategory.map((_: any, index: any) => (
+              <Cell key={`cell-${index}`} fill={expensesByCategory[index].fill} />
+            ))}
+          </Pie>
+          <Tooltip />
+        </PieChart>
+      </ResponsiveContainer>
+    </div>
   )
 }
 
