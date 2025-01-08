e2a8f6a0dd3d39c7b1f1ca32cff5782f5d4a0943
Thales Trombim <thalestr@outlook.com>
2025-01-08 20:18:29 -0300

    feat: add edit expense modal responsive

--- src/components/newComponents/ExpenseListItemMobile.tsx
+++ src/components/newComponents/ExpenseListItemMobile.tsx
@@ -1,6 +1,7 @@
 import { convertNumberToBRL } from '../../utils/monetary';
 
 import { FaTrashAlt } from "react-icons/fa";
+import { HiPencil } from "react-icons/hi";
 
 type IProps = {
   name: string
@@ -8,9 +9,10 @@ type IProps = {
   date: string
   paymentMethod?: string
   handleDeleteExpense: () => void;
+  handleEdit: () => void;
 }
 
-function ExpenseListItem({ name, amount, date, paymentMethod, handleDeleteExpense } : IProps) {
+function ExpenseListItem({ name, amount, date, paymentMethod, handleDeleteExpense, handleEdit } : IProps) {
 
   const treatedPaymentMethod = () => {
     if(paymentMethod == 'debit') return 'À vista';
@@ -29,7 +31,10 @@ function ExpenseListItem({ name, amount, date, paymentMethod, handleDeleteExpens
       <span className='w-1/3'>{ date }</span>
       <div className='flex w-1/3 items-center justify-between'>
         <span>{ treatedPaymentMethod() }</span>
-        <FaTrashAlt color='6b7280' onClick={handleDeleteExpense} className='cursor-pointer hover:!text-red-600'/>
+        <div className='flex flex-col gap-6'>
+          <HiPencil color='6b7280' size={16} className='cursor-pointer hover:!text-[#123ee5]' onClick={handleEdit} />
+          <FaTrashAlt color='6b7280' onClick={handleDeleteExpense} className='cursor-pointer hover:!text-red-600'/>
+        </div>
       </div>
     </div>
   )
